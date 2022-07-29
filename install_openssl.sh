bf5cd4f3b103abe5c2f1733666e0dbfed30ad6d9
Dimitrios <pcmagas@disroot.org>
2022-07-29 19:25:05 +0300

    Install Openssl.

--- install_openssl.sh
+++ install_openssl.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
-wget https://www.openssl.org/source/openssl-3.0.5.tar.gz 
-tar -xvf openssl-3.0.5.tar.gz
+if [[ ! -d "./cd openssl-openssl-3.0.5" ]]; then
+  wget https://github.com/openssl/openssl/archive/refs/tags/openssl-3.0.5.tar.gz
+    tar -xvf openssl-3.0.5.tar.gz
+fi
 
 echo "Makle library Path"
 mkdir -p ./lib/{32,64}
 
 echo "Install 64 bit openssl"
-cd ./openssl-3.0.5
+cd openssl-openssl-3.0.5
+make clean
 ./Configure enable-tls1_3 enable-tls1_2 --prefix=$(pwd)/../lib/64 --cross-compile-prefix=x86_64-w64-mingw32- mingw64
 make -j16
 make install
-make clean
 
+make clean
 echo "Install 32 bit openssl"
-./openssl-3.0.5/Configure enable-tls1_3 enable-tls1_2 --prefix=$(pwd)/../lib/32 --cross-compile-prefix=i686-w64-mingw32- mingw
+./Configure enable-tls1_3 enable-tls1_2 --prefix=$(pwd)/../lib/32 --cross-compile-prefix=i686-w64-mingw32- mingw
 make -j16
 make install
 
